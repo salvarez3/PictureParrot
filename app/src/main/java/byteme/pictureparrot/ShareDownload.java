dd5e03d6dda17bd7fcd7e45369e6a542f8d8bbab
Seb <s3153561@student.rmit.edu.au>
2016-08-12 01:39:18 +1000

    Image sharing feature implemented

--- app/src/main/java/byteme/pictureparrot/ShareDownload.java
+++ app/src/main/java/byteme/pictureparrot/ShareDownload.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,11 +24,26 @@ public class ShareDownload extends AppCompatActivity {
         modifiedImage = (ImageView) findViewById(R.id.modifiedImage);
 
         // GETTING THE MODIFIED IMAGE FROM INTERNAL STORAGE AND DISPLAYING IT IN THE IMAGEVIEW
-        File file = new File(android.os.Environment.getExternalStorageDirectory(), "PictureParrot-modified.jpg");
+        final File file = new File(android.os.Environment.getExternalStorageDirectory(), "PictureParrot-modified.jpg");
         Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
         modifiedImage.setImageBitmap(bitmap);
 
-        // CODE FOR SHARE OPTIONS
+        // CODE FOR SHARING THE MODIFIED IMAGE TO THIRD PARTY APPS (GMAIL, WHATSAPP TESTED SO FAR)
+        ImageButton menuItemShare = (ImageButton)findViewById(R.id.menuItemShare);
+        menuItemShare.setOnClickListener(
+                new ImageButton.OnClickListener(){
+                    public void onClick(View v){
+                        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                        Uri uri = Uri.fromFile(file);
+                        sharingIntent.setType("image/jpeg");
+                        sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here"); // MAYBE NOT NEEDED NOW?
+                        sharingIntent.putExtra(Intent.EXTRA_STREAM, uri);
+                        startActivity(Intent.createChooser(sharingIntent, "Share via"));
+                    }
+                }
+        );
+
+        /*// CODE FOR SHARING A STRING TO THIRD PARTY APPS
         ImageButton menu_item_share = (ImageButton)findViewById(R.id.menu_item_share);
         menu_item_share.setOnClickListener(
                 new ImageButton.OnClickListener(){
@@ -40,29 +56,6 @@ public class ShareDownload extends AppCompatActivity {
                         startActivity(Intent.createChooser(sharingIntent, "Share via"));
                     }
                 }
-        );
-
-        /* // Test Code
-        ImageButton menu_item_share = (ImageButton)findViewById(R.id.menu_item_share);
-        menu_item_share.setOnClickListener(
-                new ImageButton.OnClickListener(){
-                    public void onClick(View v){
-                        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-                        sharingIntent.setType("image/jpeg"); //CHANGE TEXT/PLAIN TO IMAGE? -- "image/jpeg"
-                        Image shareBody = bitmap; //CHANGE STRING TO IMAGE AND CONTENT TO MODIFIED IMAGE?
-                        sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here");
-                        sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody); //CAN I CHANGE EXTRA_TEXT TO SUIT OUT MODIFIED IMAGE?
-
-                        //TO SHARE IMAGE??
-                        sharingIntent.putExtra(Intent.EXTRA_STREAM, modifiedImage); // <<< THIS IS THE CORRECT FORMAT!! --use parse?
-                        startActivity(Intent.createChooser(sharingIntent, "Share via"));
-                    }
-                }
-        ); */
-
-
-
-
-
+        );*/
     }
 }
